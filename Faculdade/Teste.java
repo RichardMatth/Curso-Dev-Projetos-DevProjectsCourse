c520cf7f2df850c6dfc135a4f35e72e477a7f07f
Richard Matheus Farias <107589526+RichardMatth@users.noreply.github.com>
2024-06-25 10:48:29 -0300

    Update Teste.java

--- Faculdade/Teste.java
+++ Faculdade/Teste.java
@@ -5,5 +5,5 @@ public class teste {
   public static void main {
     system.out.println("Discplina: programação de sistemas.");
     system.out.print("Aprendendo a linguagem Java.\n");
-  }
-}
+  }   // Fim do metódo main
+}    // Fim da clase  "PRIMEIRO PROGRAMA"
