21feeed5dc2d50cbfd7233a74a099bd1f762379b
Richard Matheus Farias <107589526+RichardMatth@users.noreply.github.com>
2024-06-25 10:46:53 -0300

    Create Teste.java

---
+++ Faculdade/Teste.java
@@ -0,0 +1,9 @@
+// Conteúdo da disciplina "Programação de sistemas".
+// Exemplo de programa escrito em JAVA.
+
+public class teste {
+  public static void main {
+    system.out.println("Discplina: programação de sistemas.");
+    system.out.print("Aprendendo a linguagem Java.\n");
+  }
+}
