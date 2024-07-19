b94ced3126b1fef75bfede9d173e77224cc017c5
Richard Matheus Farias <107589526+RichardMatth@users.noreply.github.com>
2024-07-19 09:14:27 -0300

    Create estudo_if.java

---
+++ estudo_if.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Menor {
+    public static void main(String[] args) {
+    int num1, num2;
+    
+      Scanner teclado = new Scanner(System.in);
+    
+    System.out.print("Digite um valor inteiro: ");
+    num1 = teclado.nextInt();
+    
+    System.out.print("Digite outro valor inteiro: ");
+    num2 = teclado.nextInt();
+    
+    if (num1 < num2) {
+    System.out.println(num1 + "é menor que " + num2);
+    }
+    
+    System.out.printf("Fim do programa.");
+
+  } //fim main
+} //fim main
